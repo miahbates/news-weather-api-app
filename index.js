c0918c401ef9cdfbef5d68499871a4d89d954293
miahbates <miahbates@icloud.com>
2021-11-08 20:00:18 +0000

    set up tabbed UI for uk and US buttons in nav bar to display each section

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const tabs = document.querySelectorAll('[data-tab-target]')
+
+const tabContents = document.querySelectorAll('[data-tab-content]'); 
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener('click', () => {
+    for (let j = 0; j < tabContents.length; j++) {
+      tabContents[j].dataset.active = false;
+    }
+     const target = document.querySelector(tabs[i].dataset.tabTarget);
+     target.dataset.active = true;
+  })
+}
